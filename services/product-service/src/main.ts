48a43cfaf2e5a9b62577836c7fed3b87cc4e1953
kaitok <kaitokinjo@gmail.com>
2024-07-29 17:26:54 +0900

    add reflection

--- services/product-service/src/main.ts
+++ services/product-service/src/main.ts
@@ -3,8 +3,37 @@ import dataSource from './db'
 import { Server, ServerCredentials } from '@grpc/grpc-js'
 import { getProductServer } from './server'
 import { ProductServiceService } from '@grpc-node-server/protos/dist/product/product'
+import { ReflectionService } from '@grpc/reflection'
+import * as protoLoader from '@grpc/proto-loader'
+import * as path from 'path'
+
+// プロトコル定義のパス
+const PROTO_PATH = path.resolve(
+  __dirname,
+  '../../../packages/protos/src/product/product.proto'
+)
+
+// プロトコル定義をロード
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+  includeDirs: [
+    path.resolve(__dirname, '../../../packages/protos/dist'),
+    path.resolve(__dirname, '../../../packages/protos/src'),
+  ],
+})
 
 const server = new Server()
+
+// サービスを追加
+server.addService(ProductServiceService, getProductServer(dataSource))
+
+// リフレクションサービスを作成し、サーバーに追加
+const reflection = new ReflectionService(packageDefinition)
+reflection.addToServer(server)
 
 const HOST = process.env.HOST || '0.0.0.0'
 const PORT = Number(process.env.PORT) || 50052
@@ -13,16 +42,16 @@ const address = `${HOST}:${PORT}`
 
 dataSource
   .initialize()
-  .then((db) => {
-    server.addService(ProductServiceService, getProductServer(db))
+  .then(() => {
     server.bindAsync(
       address,
       ServerCredentials.createInsecure(),
       (error, port) => {
         if (error) {
-          throw error
+          console.error('Failed to bind server:', error)
+          return
         }
-        console.log('server is running on', port)
+        console.log('Server is running on', port)
         server.start()
       }
     )
